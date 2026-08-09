75bf91db381c6b51151aad04aafe816a3cacf1e7
rupanjali kumari <rupanjalikumari05@gmail.com>
2026-08-10 00:01:30 +0530

    Configure server port for Render

--- app.py
+++ app.py
@@ -41,5 +41,9 @@ async def predict_route(text: str):
         raise e
     
 
-if __name__=="__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+if __name__ == "__main__":
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8080))
+    )
